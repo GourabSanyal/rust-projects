0b18833c6aff80fb7a8690ad31e75dff36e8ee3f
Gourab Sanyal <sanyal.gourab@gmail.com>
2025-12-16 16:15:23 +0530

    struct with borrowed self value update

--- 100-exercises-to-learn-rust/exercises/03_ticket_v1/01_struct/src/lib.rs
+++ 100-exercises-to-learn-rust/exercises/03_ticket_v1/01_struct/src/lib.rs
@@ -5,6 +5,31 @@
 // It should also have a method named `is_available` that returns a `true` if the quantity is
 // greater than 0, otherwise `false`.
 
+struct Order {
+    price: u32,
+    quantity: u32
+}
+
+// this was the first syntax
+// impl Order {
+//     fn is_available(self) -> bool {
+//         if self.quantity > 0 {
+//             return true
+//         }
+//         false
+//     }
+// }
+
+impl Order {
+    // this borrows self
+    // hence it will use the value and not destroy the order object
+    // not using the '&self' makes sure we use self and destroy it completely
+    // hence there is no way to use Order object again
+    fn is_available(&self) -> bool { 
+        self.quantity > 0
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
