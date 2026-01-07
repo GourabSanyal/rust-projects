41ef2c16548ca2001644ab948ae11b1459c5c62d
Gourab Sanyal <sanyal.gourab@gmail.com>
2026-01-07 21:02:33 +0530

    feat: main server file update, add cargo watch

--- rust-sql-crud/src/main.rs
+++ rust-sql-crud/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+mod routes;
+mod  users;
+
+use routes::api_router;
+
+#[tokio::main]
+async fn main() {
+    let app = api_router();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    println!("server is listening to PORT 3000");
+    axum::serve(listener, app).await.unwrap();
 }
