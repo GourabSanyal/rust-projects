620f7464e126a1bb891c2c324bbc538336cbc102
GourabSanyal <sanyal.gourab@gmail.com>
2026-01-09 23:24:20 +0530

    feat: read csv with serde and serde_json add wtih parapgrah and article struct

--- read-json/src/main.rs
+++ read-json/src/main.rs
@@ -1,13 +1,41 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Deserialize, Serialize)];
+#[derive(Deserialize, Serialize)]
 struct Paragraph {
     name: String,
 }
 
-#[derive(Deserialize, Serialize)];
+#[derive(Deserialize, Serialize)]
 struct Article {
     article: String,
-    auther: String,
-    paragrapg: Vec<Paragraph>
+    author: String,
+    paragraph: Vec<Paragraph>
+}
+
+
+fn main() {
+    let json = r#"
+    {
+        "article" : "working with json in rust",
+        "author" : "Hirimba",
+        "paragraph" : [
+            {
+                "name" :  "first sentance"
+
+            },
+            {
+                "name" : "paragraph body"
+            },
+            {
+                "name":"this is the third sentacne"
+            }
+            ]
+        }"#;
+        let parsed : Article = read_json_types(json);
+        println!("/n/n The name of the first paragraph is: {}", parsed.paragraph[0].name);
+}
+
+fn read_json_types(raw_json: &str) -> Article {
+    let parsed: Article = serde_json::from_str(raw_json).unwrap();
+    return parsed;
 }
