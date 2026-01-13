f139db42d53fc031afe414cd13f2bc8606304161
Gourab Sanyal <sanyal.gourab@gmail.com>
2026-01-13 13:24:58 +0530

    feat: update main.rs to use DATAVSE_URL using dotenv from .env file, integrated postgress connection using PgPool with api_router() to make API calls

--- rust-sql-crud/src/main.rs
+++ rust-sql-crud/src/main.rs
@@ -2,10 +2,21 @@ mod routes;
 mod  users;
 
 use routes::api_router;
+use std::env;
+use dotenv::dotenv;
+use sqlx::PgPool;
+use axum::Router;
 
 #[tokio::main]
 async fn main() {
-    let app = api_router();
+    dotenv::dotenv().ok();
+
+    let pool = PgPool::connect(&env::var("DATABASE_URL").unwrap())
+        .await
+        .unwrap();
+
+    let app = api_router().with_state(pool);
+
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     println!("server is listening to PORT 3000");
     axum::serve(listener, app).await.unwrap();
