c0abb9eeda090c1198bee46e8af382076f15bae8
Gourab Sanyal <sanyal.gourab@gmail.com>
2026-01-13 13:22:52 +0530

    feat: update Router in global users route to accept PgPool

--- rust-sql-crud/src/routes/mod.rs
+++ rust-sql-crud/src/routes/mod.rs
@@ -1,7 +1,8 @@
 use axum::Router;
 use crate::users::routes::user_router;
+use sqlx::PgPool;
 
-pub fn api_router() -> Router {
+pub fn api_router() -> Router<PgPool> {
     Router::new()
         .nest("/users", user_router())
 }
