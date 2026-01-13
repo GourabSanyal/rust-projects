d81234d4a8c23ac0494c73ed832a1e20baaebd39
Gourab Sanyal <sanyal.gourab@gmail.com>
2026-01-13 13:22:06 +0530

    feat: update Router return in "/users" modular route to handle PgPool

--- rust-sql-crud/src/users/routes.rs
+++ rust-sql-crud/src/users/routes.rs
@@ -1,10 +1,8 @@
-// exports user route
-//then imports in routes/mod.rs
-
 use axum::{Router, routing::{get, post, put, delete}};
 use crate::users::handlers::{root, create, all_users, update, delete_user};
+use sqlx::PgPool;
 
-pub fn user_router() -> Router {
+pub fn user_router() -> Router<PgPool> {
     Router::new()
         .route("/", get(root))
         .route("/create", post(create))
