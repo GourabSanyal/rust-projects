01231c309a68f7219b779d04355c14589040db0c
Gourab Sanyal <sanyal.gourab@gmail.com>
2026-02-04 23:49:33 +0530

    my solution for traits outro

--- 100-exercises-to-learn-rust/exercises/04_traits/14_outro/src/lib.rs
+++ 100-exercises-to-learn-rust/exercises/04_traits/14_outro/src/lib.rs
@@ -1,4 +1,4 @@
-// TODO: Define a new `SaturatingU16` type.
+// TODO: Define a from `SaturatingU16` type.
 //   It should hold a `u16` value.
 //   It should provide conversions from `u16`, `u8`, `&u16` and `&u8`.
 //   It should support addition with a right-hand side of type
@@ -8,3 +8,74 @@
 //   It should be possible to print its debug representation.
 //
 // Tests are located in the `tests` folder—pay attention to the visibility of your types and methods.
+
+use std::convert::From;
+use std::ops::Add;
+
+#[derive(PartialEq, Debug, Copy, Clone)]
+pub struct SaturatingU16 {
+    value: u16
+}
+
+impl SaturatingU16 {
+    pub fn new(value: u16) -> Self{
+        Self {value}
+    }
+}
+
+impl From<u16> for SaturatingU16 {
+    fn from(value: u16) -> Self{
+        SaturatingU16 {value}
+    }
+}
+
+impl From<u8> for SaturatingU16 {
+    fn from(value: u8) -> Self{
+        SaturatingU16 {value: value.into()}
+    }
+}
+
+impl From<&u16> for SaturatingU16 {
+    fn from(value: &u16) -> Self{
+        SaturatingU16 {value: *value}
+    }
+}
+
+impl From<&u8> for SaturatingU16 {
+    fn from(value: &u8) -> Self{
+        SaturatingU16 {value: (*value).into()}
+    }
+}
+
+impl Add for SaturatingU16{
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self {
+        Self::from(self.value.saturating_add(rhs.value))
+    }
+}
+
+impl Add<u16> for SaturatingU16{
+    type Output = u16;
+
+    fn add(self, rhs: u16) -> Self::Output {
+        self.value.saturating_add(rhs)
+    }
+}
+
+impl Add<&u16> for SaturatingU16{
+    type Output = SaturatingU16;
+
+    fn add(self, rhs: &u16) -> Self::Output {
+        Self::from(self.value.saturating_add(*rhs))
+    }
+}
+
+impl Add<&SaturatingU16> for SaturatingU16{
+    type Output = SaturatingU16;
+
+    fn add(self, rhs: &SaturatingU16) -> Self::Output {
+        Self::from(self.value.saturating_add(rhs.value))
+    }
+}
+
