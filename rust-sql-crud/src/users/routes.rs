0282fc5c98aef12ce1107953b7067815b8bf38f9
Gourab Sanyal <sanyal.gourab@gmail.com>
2026-02-19 23:56:57 +0530

    bug: fix export error for delete_user route

--- rust-sql-crud/src/users/routes.rs
+++ rust-sql-crud/src/users/routes.rs
@@ -2,7 +2,7 @@ use axum::{Router, routing::{get, post, put, delete}};
 use crate::users::handlers::{root_path, create, 
     all_users, 
     // update, 
-    delete
+    delete_user
 };
 use sqlx::PgPool;
 
@@ -12,5 +12,5 @@ pub fn user_router() -> Router<PgPool> {
         .route("/create", post(create))
         .route("/getallusers", get(all_users))
         // .route("/update/:id", put(update))
-        .route("/delete/:id", delete(delete))
+        .route("/delete/:id", delete(delete_user))
 }
